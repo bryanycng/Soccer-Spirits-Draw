cf793800d7aabbb9275127fe00b5e19037e4d71d
Bryan Ng <bryan.yc.ng@shaw.ca>
2016-07-06 01:02:43 -0700

    Added images

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,39 +26,89 @@ public class Main {
 //		System.out.println(p.name + " " + p.element + " " + p.star + " " + p.probability);
 //		}
 		
-		for (int i = 0; i < 10; i++){
-		System.out.println(Player.drawPlayer());
-		}
+//		for (int i = 0; i < 3; i++){
+//		System.out.println(Player.drawPlayer().name);
+//		}
 //	
+		
 		JFrame frame = new JFrame("Soccer Spirits Draw Simultor");
-		final JPanel openingPanel = new JPanel();
-		openingPanel.setSize(900, 600);
-		openingPanel.setLayout(new FlowLayout());
+		frame.setSize(900, 600);
 
+		BufferedImage myPicture = ImageIO.read(new File("pictures/whiteBackground.jpg"));
+		Image scaledImage = myPicture.getScaledInstance(frame.getWidth()/4,frame.getHeight()/4,Image.SCALE_SMOOTH);
+		JLabel picLabel = new JLabel(new ImageIcon(scaledImage));
+		
+		
+		JPanel drawPanel = new JPanel();
+
+		final JLabel card0 = new JLabel(new ImageIcon(scaledImage));
+		final JLabel card1 = new JLabel(new ImageIcon(scaledImage));
+		final JLabel card2 = new JLabel(new ImageIcon(scaledImage));
+		
 		JLabel label = new JLabel("Normal 10 draw!");
-
 		JButton draw = new JButton();
 		draw.setText("Draw");
 		draw.addActionListener(new ActionListener() {
 			  public void actionPerformed(ActionEvent evt) {
-				  for (int i = 0; i < 10; i++){
-						System.out.println(Player.drawPlayer());
+					String name1 = Player.drawPlayer().name;
+					String name2 = Player.drawPlayer().name;
+					String name3 = Player.drawPlayer().name;
+					System.out.println(name1);
+//						System.out.println(Player.drawPlayer().name);
+						try {
+							BufferedImage bi = getBufferedImagePNG(name1);
+							card0.setIcon(setImage(bi));
+						} catch (IOException e) {
+							try {
+								BufferedImage bi = getBufferedImageJPG(name1);
+							} catch (IOException e1) {							
+							}
+							e.printStackTrace();
 						}
-			  }
-			});
+			  		}
+				});		
+		drawPanel.add(label);
+		drawPanel.add(draw);
+
 		
 		
-		BufferedImage myPicture = ImageIO.read(new File("pictures/background.jpg"));
-		Image scaledImage = myPicture.getScaledInstance(openingPanel.getWidth(),openingPanel.getHeight(),Image.SCALE_SMOOTH);
-		JLabel picLabel = new JLabel(new ImageIcon(scaledImage));
-		openingPanel.add(picLabel);
-		openingPanel.add(label);
-		openingPanel.add(draw);
-		frame.add(openingPanel);
-		frame.setSize(900, 600);
-		frame.setLocationRelativeTo(null);
+	
+
+
+	
+		frame.add(drawPanel, BorderLayout.PAGE_START);
+		//frame.add(picLabel, BorderLayout.CENTER);
+		frame.add(card0, BorderLayout.LINE_START);
+		frame.add(card1, BorderLayout.CENTER);
+		frame.add(card2, BorderLayout.LINE_END);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		frame.setResizable(false);
+		
+
+		
+	}
+	
+	
+	public static BufferedImage getBufferedImageJPG(String name) throws IOException{
+		BufferedImage buffered = ImageIO.read(new File("pictures/" + name + ".jpg"));
+		return buffered;
+	}
+	public static BufferedImage getBufferedImagePNG(String name) throws IOException{
+		BufferedImage buffered = ImageIO.read(new File("pictures/" + name + ".png"));
+		return buffered;
+	}
+	
+	public static ImageIcon setImage(BufferedImage pic){
+		Image scaledImage = pic.getScaledInstance(200,200,Image.SCALE_SMOOTH);
+		ImageIcon imageIcon = new ImageIcon(scaledImage);
+		return imageIcon;
+	}
+	
+	public void setDrawImage(BufferedImage bi){
+		
+		
 	}
 
 
